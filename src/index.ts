0b6b87b2c7b5a2c5e821939278d9fb32e532543c
soldevkit <219002813+soldevkit@users.noreply.github.com>
2025-09-16 20:17:41 +0700

    iconify marketcap (MC) & show volume (V)

--- src/index.ts
+++ src/index.ts
@@ -7,15 +7,22 @@ import { PumpfunGraduatedCoins } from './types';
 TimeAgo.addDefaultLocale(en);
 const timeAgo = new TimeAgo('en-US');
 
+const MIN_HOLDERS = 333;
+const MAX_SNIPERS_PCT = 3;
+const MAX_TOP_HOLDERS_PCT = 33;
+const MIN_MC = 3333;
+const MC_ATH_THRESHOLD = 0.6; // 60%
+
 (async () => {
   const res = await fetch(
-    'https://advanced-api-v2.pump.fun/coins/graduated?sortBy=creationTime&marketCapFrom=33333&numHoldersFrom=333&snipersOwnedPercentageTo=3&topTenHoldersOwnedPercentageTo=33',
+    `https://advanced-api-v2.pump.fun/coins/graduated?sortBy=creationTime&marketCapFrom=${MIN_MC}&numHoldersFrom=${MIN_HOLDERS}&snipersOwnedPercentageTo=${MAX_SNIPERS_PCT}&topTenHoldersOwnedPercentageTo=${MAX_TOP_HOLDERS_PCT}`,
   );
   const data: PumpfunGraduatedCoins = await res.json();
 
   const printData: (string | number)[][] = [
     [
       'Mint',
+      'V (SOL)',
       'MC',
       'ATH',
       'Holders',
@@ -30,25 +37,37 @@ const timeAgo = new TimeAgo('en-US');
   ];
 
   printData.push(
-    ...data.coins.map((coin) => [
-      coin.coinMint,
-      millify(coin.marketCap),
-      millify(coin.allTimeHighMarketCap),
-      coin.numHolders.toLocaleString('en-US'),
-      coin.sniperOwnedPercentage.toLocaleString('en-US', {
-        style: 'percent',
-        maximumFractionDigits: 2,
-      }),
-      (coin.topHoldersPercentage / 100).toLocaleString('en-US', {
-        style: 'percent',
-        maximumFractionDigits: 2,
-      }),
-      coin.hasWebsite ? 'Y' : 'N',
-      coin.hasTwitter ? 'Y' : 'N',
-      coin.hasTelegram ? 'Y' : 'N',
-      coin.hasSocial ? 'Y' : 'N',
-      timeAgo.format(coin.graduationDate),
-    ]),
+    ...data.coins
+      .filter((coin) => coin.marketCap > coin.allTimeHighMarketCap * MC_ATH_THRESHOLD)
+      .map((coin) => [
+        coin.coinMint,
+        millify(coin.volume),
+        millify(coin.marketCap) +
+          (coin.marketCap > 1000000
+            ? ' 🔥'
+            : coin.marketCap > 500000
+              ? ' 🚀'
+              : coin.marketCap > 250000
+                ? ' 💎'
+                : coin.marketCap > 125000
+                  ? ' 🌱'
+                  : ''),
+        millify(coin.allTimeHighMarketCap),
+        coin.numHolders.toLocaleString('en-US') + (coin.numHolders > 1000 ? ' 🏆' : ''),
+        coin.sniperOwnedPercentage.toLocaleString('en-US', {
+          style: 'percent',
+          maximumFractionDigits: 2,
+        }),
+        (coin.topHoldersPercentage / 100).toLocaleString('en-US', {
+          style: 'percent',
+          maximumFractionDigits: 2,
+        }),
+        coin.hasWebsite ? 'Y' : 'N',
+        coin.hasTwitter ? 'Y' : 'N',
+        coin.hasTelegram ? 'Y' : 'N',
+        coin.hasSocial ? 'Y' : 'N',
+        timeAgo.format(coin.graduationDate),
+      ]),
   );
 
   console.log(table(printData));
